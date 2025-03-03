094ccd36a9379f27f4b688e38704117f1236ad14
Rahul Kumar Saini <sainirahul8967@gmail.com>
2025-03-04 02:18:50 +0530

    remove firebase error

--- src/firebase.js
+++ src/firebase.js
@@ -4,21 +4,21 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtn_eAuLPDB1ML1PLAQmKlm_cpvLkvgmQ",
-  authDomain: "financly-fa2db.firebaseapp.com",
-  projectId: "financly-fa2db",
-  storageBucket: "financly-fa2db.appspot.com",
-  messagingSenderId: "483707680125",
-  appId: "1:483707680125:web:b8cc599d6733c3d8434814",
-  measurementId: "G-PNJ3FHL8C1"
+  apiKey: "AIzaSyBenoXrJjoIQTfY1C_aN8i_7pO_bm2nuxQ",
+  authDomain: "financly-82bcc.firebaseapp.com",
+  projectId: "financly-82bcc",
+  storageBucket: "financly-82bcc.firebasestorage.app",
+  messagingSenderId: "772222871189",
+  appId: "1:772222871189:web:76cf6ca09d43b0217ddd9c",
+  measurementId: "G-E61B5CBJKD",
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-// eslint-disable-next-line no-unused-vars
 const analytics = getAnalytics(app);
 
 export { db, auth, provider, doc, setDoc };
